906c564246739035ab6eeaf952a00231b0019ba4
aniljos <anil.jos@gmail.com>
2025-02-14 09:44:47 +0530

    app created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default async function AboutPage(){
+
+    
+    return (
+        <div>
+            <h3>About</h3>
+            <p>This is Next.js 15 application</p>
+        </div>
+    )
+}
